d5338a79e14c17c7d0403679bf62382284a583f9
Iaroslav <yslavcom.mon@gmail.com>
2024-10-27 09:20:05 +1100

    setting up flask

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,32 +1,43 @@
 # Import the dependencies.
+from flask import Flask
 
-
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # Declare a Base using `automap_base()`
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__main__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+
+if __main__ == '__main__':
+    app.run(debug=True)
